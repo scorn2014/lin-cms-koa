2ed2025de9e4cc819c333b87c273b662a5f8248b
scorn2014 <tftrack@163.com>
2020-09-04 11:43:06 +0800

    ssh config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,11 +13,10 @@ module.exports = {
   deploy: {
     production: {
       user: "root",
-      host: "119.28.221.166",
+      host: "tencent_HK",
       ref: "origin/master",
       repo: "git@github.com:scorn2014/lin-cms-koa.git",
       path: "/root/app/lin-cms-koa",
-      "pre-deploy-local": "ssh tencent_HK",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
